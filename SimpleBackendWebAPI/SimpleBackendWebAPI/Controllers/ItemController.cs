5c1f4bb8beb1998888f1fe79c1f5ce3236db44ac
Ashmoon32 <nyeinmaung3021@gmail.com>
2025-12-14 20:49:36 +0630

    -

---
+++ SimpleBackendWebAPI/SimpleBackendWebAPI/Controllers/ItemController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleBackendWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemController : ControllerBase
+    {
+        // Simple in-memory storage
+        private static List<Item> _items = new List<Item>
+        {
+            new Item { Id = 1, Name = "Setup Visaul Studio" }
+        };
+
+        [HttpGet]
+        public IActionResult GetItems()
+        {
+            return Ok(_items);
+        }
+
+        [HttpPost]
+        public IActionResult AddItem([FromBody] Item newItem){
+            newItem.Id = _items.Count > 0 ? _items.Max(i => i.Id) + 1 : 1;
+            _items.Add(newItem);
+            return Ok(newItem);
+            }
+
+    }
+}
--- TextBasedRPGGame/TextBasedRPGGame/Action.cs
+++ TextBasedRPGGame/TextBasedRPGGame/Action.cs
@@ -10,6 +10,7 @@ namespace TextBasedRPGGame
             int PlayerHealth = 100;
             int PlayerMana = 100;
             int PlayerStamina = 50;
+            int PlayerCounterAttackPerHit = 5;
             int NormalMonsterHealth = 50;
             int NormalMonsterPowerPerHit = 5;
             int MagicMonsterHealth = 30;
@@ -17,6 +18,7 @@ namespace TextBasedRPGGame
             int BossMonsterHealth = 200;
             int BossMonsterPowerPerHit = 20;
             int BossMonsterSpecialPower = 50;
+            bool isPlayerAlive = true;
             string CharacterName;
 
             // To check user only enter alphabetical characters
@@ -44,47 +46,142 @@ namespace TextBasedRPGGame
                 }
             }
 
-            string choice;
 
-            Console.WriteLine("Choose your action");
-            Console.WriteLine("==================================");
-            Console.Write("1.Attack , 2.Run, 3.Defend, 4.Use Ultimate (Default is attack!): ");
-
-            choice = Console.ReadLine();
-            int intChoice = Convert.ToInt32(choice);
-
-
-            switch (intChoice)
+            try
             {
-                case 1:
-                    Console.WriteLine("You attack the Monster! 10% of Monster's health decreases.");
-                    NormalMonsterHealth -= 10;
-                    PlayerMana -= 10;
-                    break;
+                while (isPlayerAlive)
+                {
+                    // Logic for Normal Monster Case
+                    // logic - monster will attack when monsterCounterAttack is divisible by 2, otherwise player attack
+                    int monsterCounterAttack;
 
-                case 2:
-                    Console.WriteLine("You run and escape from monster attack!");
-                    PlayerStamina -= 10;
-                    break;
+                    for (monsterCounterAttack = 1; monsterCounterAttack <= 10; monsterCounterAttack++)
+                    {
+                        if (monsterCounterAttack % 2 == 0)
+                        {
+                            string counterChoice;
+                            Console.WriteLine("Monster Attacks You! What is your counter actions? Choose your action");
+                            Console.WriteLine("==================================");
+                            Console.Write("1. Attack back, 2. Defend, 3. Run (Default is defend!): ");
 
-                case 3:
-                    Console.WriteLine("You defend the monster attack!");
-                    PlayerMana -= 10;
-                    break;
+                            counterChoice = Console.ReadLine();
+                            int intCounterChoice = Convert.ToInt32(counterChoice);
+                            if (intCounterChoice == 1 || intCounterChoice == 2 || intCounterChoice == 3)
+                            {
+                                switch (intCounterChoice)
+                                {
+                                    case 1:
+                                        Console.WriteLine("You attack the counter attack while monster is attacking you! 5% of Monster's and 5% of you health also decreases.");
+                                        NormalMonsterHealth -= PlayerCounterAttackPerHit;
+                                        PlayerHealth -= NormalMonsterPowerPerHit;
+                                        PlayerMana -= 10;
+                                        break;
 
-                case 4:
-                    Console.WriteLine("Awesome Attack!! You use you ultimate power!! The monster health decreases 50%.");
-                    NormalMonsterHealth -= 50;
-                    PlayerMana -= 30;
-                    break;
+                                    case 2:
+                                        Console.WriteLine("You defend the monster attack! The monster attack isn't effected to you.");
+                                        PlayerMana -= 10;
+                                        break;
 
-                default:
-                    Console.WriteLine("You attack the Monster! 10% of Monster's health decreases.");
-                    NormalMonsterHealth -= 10;
-                    PlayerMana -= 10;
-                    break;
+                                    case 3:
+                                        Console.WriteLine("You run and escape from monster attack! No monster attack hits for a while.");
+                                        PlayerStamina -= 10;
+                                        break;
+
+                                    default:
+                                        Console.WriteLine("You defend the monster attack! The monster attack isn't effected to you.");
+                                        PlayerMana -= 10;
+                                        break;
+                                }
 
 
+                            }
+                            else
+                            {
+                                Console.WriteLine("Invalid choice! Defaul action 'Defend' is selected.");
+                                Console.WriteLine("You defend the monster attack! The monster attack isn't effected to you.");
+                                PlayerMana -= 10;
+                            }
+
+                            Console.WriteLine($"Your Health: {PlayerHealth}%, Mana: {PlayerMana}%, Stamina: {PlayerStamina}%");
+                            Console.WriteLine($"Monster Health: {NormalMonsterHealth}%");
+                        }
+                        else
+                        {
+                            while (monsterCounterAttack % 2 != 0 && NormalMonsterHealth > 0)
+                            {
+                                string choice;
+                                Console.WriteLine("Choose your action");
+                                Console.WriteLine("==================================");
+                                Console.Write("1.Attack , 2.Run, 3.Defend, 4.Use Ultimate (Default is attack!): ");
+
+                                choice = Console.ReadLine();
+                                int intChoice = Convert.ToInt32(choice);
+
+                                if (intChoice == 1 || intChoice == 2 || intChoice == 3 || intChoice == 4)
+                                {
+                                    switch (intChoice)
+                                    {
+                                        case 1:
+                                            Console.WriteLine("You attack the Monster! 10% of Monster's health decreases.");
+                                            NormalMonsterHealth -= 10;
+                                            PlayerMana -= 10;
+                                            break;
+
+                                        case 2:
+                                            Console.WriteLine("You run and escape from monster attack! No Monster Attack hits.");
+                                            PlayerStamina -= 10;
+                                            break;
+
+                                        case 3:
+                                            Console.WriteLine("You defend the monster attack! The monster attack isn't effected to you.");
+                                            PlayerMana -= 10;
+                                            break;
+
+                                        case 4:
+                                            Console.WriteLine("Awesome Attack!! You use you ultimate power!! The monster health is decreased 50%.");
+                                            NormalMonsterHealth -= 50;
+                                            PlayerMana -= 30;
+                                            break;
+
+                                        default:
+                                            Console.WriteLine("You attack the Monster! 10% of Monster's health is decreased.");
+                                            NormalMonsterHealth -= 10;
+                                            PlayerMana -= 10;
+                                            break;
+                                    }
+
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Invalid choice! Default action 'Attack' is selected.");
+                                    Console.WriteLine("You attack the Monster! 10% of Monster's health is decreased.");
+                                    NormalMonsterHealth -= 10;
+                                    PlayerMana -= 10;
+                                }
+
+                                Console.WriteLine($"Your Health: {PlayerHealth}%, Mana: {PlayerMana}%, Stamina: {PlayerStamina}%");
+                                Console.WriteLine($"Monster Health: {NormalMonsterHealth}%");
+                                break;
+
+                            }
+                        }
+
+                    }
+
+
+                    if (PlayerHealth <= 0)
+                    {
+                        isPlayerAlive = false;
+
+                    }
+                }
+
+                Console.WriteLine("Game Over! You have been defeated by the monster. See you next time, brave legend " + CharacterName + "...");
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Unexpected Error Occurred! Please Try Again.");
             }
         }
     }
